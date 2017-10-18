9dd286bc38c2d6ec3e1d8b7aae5ec77a480caac2
sintiadewi611 <32576275+sintiadewi611@users.noreply.github.com>
2017-10-18 20:08:39 +0700

    Add files via upload

---
+++ Menanaknasi.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package menanaknasi;
+
+/**
+ *
+ * @author Sintia Dewi
+ */
+public class Menanaknasi {
+
+    public static void main(String[] args) {
+        System.out.println("TUGAS OOP");
+        System.out.println("Sintia Dewi");
+        System.out.println("Cara Menanak Nasi di Rice Cooker");
+        System.out.println("1. Ukur beras dengan cangkir takar, dan masukkan ke dalam panci penanak nasi Anda.");
+        System.out.println("2. Cuci beras yang telah ditakar");
+        System.out.println("3. Ukur air. Banyak air yang anda tambahkan tergantung pada jumlah dan jenis beras yang anda masak, serta tekstur nasi seperti apa yang anda inginkan. ");
+        System.out.println("4. Rendam beras selama 30 menit (Sesuai selera) ");
+        System.out.println("5. Tambahkan bumbu (Sesuai selera) ");
+        System.out.println("6. Periksa penanak nasi anda untuk menyalakan pilihan atau pengaturan khusus");
+        System.out.println("7. Masak nasi dalam penanak nasi, dengan menekan tombol untuk memasak (cook) ");
+        System.out.println("8. Biarkan nasi selama 10-20 menit sampai matang dengan tanda bunyi pada tombol warm");
+        System.out.println("9. Aduk nasi agak tidak padat dan menggumpal, kemudian sajikan");
+        System.out.println("");
+    }
+    
+}
